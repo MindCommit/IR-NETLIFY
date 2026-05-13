4573cda08a8c7a32d7648a81e675916d15624835
IR NETLIFY <mehrzadsivizi@gmail.com>
2026-05-13 14:17:10 +0330

    Update generator.py

--- generator.py
+++ generator.py
@@ -6,10 +6,24 @@ import random
 
 def create_vless(user, address, sni, host, path, remark, x_host=None):
     base_path = urllib.parse.quote(path, safe="")
-    params = f"encryption=none&security=tls&sni={sni}&alpn=h2%2Chttp%2F1.1&insecure=1&allowInsecure=1&type=xhttp&host={host}&path={base_path}&mode=auto"
+    
+    extra_data = {
+        "xPaddingBytes": "1-1",
+        "scMaxEachPostBytes": "1000000",
+        "headers": {}
+    }
+    
     if x_host:
-        extra_json = json.dumps({"headers": {"x-host": x_host}}, separators=(",", ":"))
-        params += f"&extra={urllib.parse.quote(extra_json)}"
+        extra_data["headers"]["x-host"] = x_host
+    
+    params = (
+        f"encryption=none&security=tls&sni={sni}&alpn=h2%2Chttp%2F1.1"
+        f"&insecure=1&allowInsecure=1&type=xhttp&host={host}&path={base_path}&mode=auto"
+    )
+    
+    extra_json = json.dumps(extra_data, separators=(",", ":"))
+    params += f"&extra={urllib.parse.quote(extra_json)}"
+    
     return f"vless://{user}@{address}:443?{params}#{urllib.parse.quote(remark)}"
 
 
@@ -17,18 +31,21 @@ def run():
     servers = [
         {"name": "ParsaServer", "address": "netlify.parsashonam.sbs:444", "path": "/p4r34m", "user": "Telegram-Parsashonam"},
         {"name": "Xarheon", "address": "https://xarheon.run.place", "path": "/cdn2", "user": "f9b57060-989a-4d6d-bc58-53d767242968"},
-        {"name": "BEHNAM", "address": "s1.onesrv98.eu.cc:444", "path": "/mine", "user": "a5c04120-8f0d-4d52-8cdd-9ffff79a777d"},
-        {"name": "Flare", "address": "https://flare.diablo0.site:24855", "path": "/free", "user": "dab65f2a-48ca-4fe8-9143-06cd28f00a21"},
-        {"name": "Alwinbalot", "address": "https://solar.latnashobivojod.shop:8096", "path": "/alwin", "user": "ccee709a-5766-4ced-a6ec-7b9d46cdd48d"},
+ #       {"name": "BEHNAM", "address": "s1.onesrv98.eu.cc:444", "path": "/mine", "user": "a5c04120-8f0d-4d52-8cdd-9ffff79a777d"},
+ #       {"name": "Flare", "address": "https://flare.diablo0.site:24855", "path": "/free", "user": "dab65f2a-48ca-4fe8-9143-06cd28f00a21"},
+        {"name": "Alwinbalot", "address": "https://solar.latnashobivojod.shop:443", "path": "/jhiuiu", "user": "5cefde6e-867f-452d-8667-d30b01c63050"},
         {"name": "Mostafa", "address": "http://1.gymnation.ir:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
         {"name": "Farshid", "address": "http://aa.websooj.site:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
-        {"name": "mohsen", "address": "http://150.241.91.212:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
+#        {"name": "mohsen", "address": "http://150.241.91.212:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
         {"name": "shabgard", "address": "http://cdn.lindasannp.com:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
-        {"name": "Pouya", "address": "http://76.13.14.51:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
-        {"name": "Masoud", "address": "http://104.234.135.93:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
+#        {"name": "Pouya", "address": "http://76.13.14.51:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
+#        {"name": "Masoud", "address": "http://104.234.135.93:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
         {"name": "Navid", "address": "https://irnetlify.navtechz.com:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
         {"name": "AbolUp", "address": "http://sub.abolup.online:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
         {"name": "Hossein", "address": "http://str.fiathusen.homes:444", "path": "/IR_NETLIFY", "user": "598cbaec-00f6-4d84-8644-b35fdcec6904"},
+        {"name": "Saman 2", "address": "https://ppp.sam4.lol:30903", "path": "/asekhi", "user": "132f0ba3-ebbe-4153-84ff-63161b841ed8"},
+        {"name": "Ali", "address": "http://185.181.208.92:444", "path": "/IR_NETLIFY", "user": "%40IR_NETLIFY"},
+        
     ]
 
     new_snis = ["kubernetes.io", "kueue.sigs.k8s.io", "vuejs.org"]
@@ -43,13 +60,10 @@ def run():
             h_old = f.read().strip()
         if h_old:
             res = [create_vless("Telegram-Parsashonam", s, s, h_old, "/p4r34m", f"🌐 @IR_NETLIFY   {s}") for s in old_direct_snis]
-            
             chosen_old_snis = random.sample(old_combo_snis, min(len(old_combo_snis), 5))
             chosen_old_ips = random.sample(old_ips, min(len(old_ips), 10))
-            
             res.extend([create_vless("Telegram-Parsashonam", ip, s, h_old, "/p4r34m", f"🌐 @IR_NETLIFY  {s}  {ip}") 
                         for s in chosen_old_snis for ip in chosen_old_ips])
-            
             with open("IR_NETLIFY_SUB.txt", "w") as f:
                 f.write("\n".join(res))
 
@@ -58,7 +72,6 @@ def run():
             h_new = f.read().strip()
         if h_new:
             res_new = []
-            
             direct_servers = random.sample(servers, min(len(servers), 3))
             for srv in direct_servers:
                 res_new.extend([create_vless(srv["user"], s, s, h_new, srv["path"], f"🌐 @IR_NETLIFY | {s} | {srv['name']}", srv["address"]) 
